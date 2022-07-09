ba4572bc2ced1f92a493e1a76a872d1710a774d5
zagoruev <zagoruev@gmail.com>
2022-07-09 20:23:13 +0300

    Create models

---
+++ app/Models/Article.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Article extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'body', 'img', 'slug'];
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function state(): HasOne
+    {
+        return $this->hasOne(State::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+}
